8ce0b47951a853d97b7197d77d470bc9804b97fc
Victor Sui <victorsui11@gmail.com>
2018-04-14 22:33:08 -0500

    Initial commit for web client

---
+++ web/src/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+render(
+  <div>Dripdripdrip....</div>,
+  document.getElementById('app')
+);
+
+module.hot.accept();
