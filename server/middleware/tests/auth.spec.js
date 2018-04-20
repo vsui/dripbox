f00c8ce07a3eed7a9125681789cdb631628a4454
Victor Sui <victorsui11@gmail.com>
2018-04-19 19:43:05 -0500

    Add auth tests

--- server/middleware/tests/auth.spec.js
+++ server/middleware/tests/auth.spec.js
@@ -1,9 +1,16 @@
+jest.mock('bcrypt', () => ({
+  genSalt: jest.fn(),
+  hash: jest.fn().mockResolvedValue('hashXXX'),
+  compare: jest.fn(),
+}));
+jest.mock('jsonwebtoken', () => ({
+  sign: jest.fn(),
+}));
+
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-const mockStore = jest.mock();
-
-const { login, register, verify } = require('../auth')(mockStore);
+const auth = require('../auth');
 
 const ctxWithCredentials = () => ({
   request: {
@@ -12,18 +19,26 @@ const ctxWithCredentials = () => ({
       password: 'gbiv',
     },
   },
+  headers: {
+    authorization: 'Bearer blah',
+  },
+  params: {
+
+  },
 });
 
 describe('login middleware', () => {
   it('should set status to 401 if user with username is not found', async () => {
-    mockStore.findOne = jest.fn().mockResolvedValue(null);
+    const mockStore = { findOne: jest.fn().mockResolvedValue(null) };
+    const { login } = auth(mockStore);
     const ctx = ctxWithCredentials();
     await login(ctx, null);
     expect(ctx.status).toBe(401);
   });
 
   it('should set status to 401 if jwt is not properly authenticated', async () => {
-    mockStore.findOne = jest.fn().mockResolvedValue({});
+    const mockStore = { findOne: jest.fn().mockResolvedValue({}) };
+    const { login } = auth(mockStore);
     bcrypt.compare = jest.fn().mockResolvedValue(false);
     const ctx = ctxWithCredentials();
     await login(ctx, null);
@@ -31,8 +46,9 @@ describe('login middleware', () => {
   });
 
   it('should call next if jwt is properly authenticated', async () => {
+    const mockStore = { findOne: jest.fn().mockResolvedValue({}) };
+    const { login } = auth(mockStore);
     jwt.sign = jest.fn();
-    mockStore.findOne = jest.fn().mockResolvedValue({});
     bcrypt.compare = jest.fn().mockResolvedValue(true);
     const mockNext = jest.fn();
     const ctx = ctxWithCredentials();
@@ -40,8 +56,10 @@ describe('login middleware', () => {
     expect(mockNext).toHaveBeenCalledTimes(1);
   });
   it('should set status to 200 if jwt is properly authenticated', async () => {
+    const mockStore = { findOne: jest.fn().mockResolvedValue({}) };
+    const { login } = auth(mockStore);
     jwt.sign = jest.fn();
-    mockStore.findOne = jest.fn().mockResolvedValue({});
+    mockStore.findOne = jest.fn().mockResolvedValue({ username: 'roy' });
     bcrypt.compare = jest.fn().mockResolvedValue(true);
     const mockNext = jest.fn();
     const ctx = ctxWithCredentials();
@@ -49,8 +67,9 @@ describe('login middleware', () => {
     expect(ctx.status).toBe(200);
   });
   it('should set body to token if jwt is properly authenticated', async () => {
+    const mockStore = { findOne: jest.fn().mockResolvedValue({}) };
+    const { login } = auth(mockStore);
     jwt.sign = jest.fn().mockReturnValue('mockToken');
-    mockStore.findOne = jest.fn().mockResolvedValue({});
     bcrypt.compare = jest.fn().mockResolvedValue(true);
     const mockNext = jest.fn();
     const ctx = ctxWithCredentials();
@@ -59,20 +78,138 @@ describe('login middleware', () => {
   });
 });
 
-describe.skip('register middleware', () => {
-});
+describe('register middleware', () => {
+  beforeEach(() => jest.clearAllMocks());
 
-describe.skip('verify middleware', () => {
-  beforeEach(() => {
-    mockStore.clearMock();
+  it('should successfully register if the user does not exist ', async () => {
+    const ctx = ctxWithCredentials();
+    const mockStore = {
+      findOne: jest.fn().mockResolvedValue(null),
+      insertOne: jest.fn(),
+    };
+    const { register } = auth(mockStore);
+    await register(ctx, jest.fn());
+    expect(mockStore.insertOne).toHaveBeenCalledWith({ username: 'roy', hash: 'hashXXX' });
   });
 
-  it('should set status to 401 if user is not found', () => {
-    const ctx = {};
-    verify(ctx, null);
+  it('should return token if the user does not exist', async () => {
+    const ctx = ctxWithCredentials();
+    const mockStore = {
+      findOne: jest.fn().mockResolvedValue(null),
+      insertOne: jest.fn(),
+    };
+    const { register } = auth(mockStore);
+    await register(ctx, jest.fn());
+    expect(ctx.body).toEqual({ token: 'mockToken' });
+  });
+
+  it('should set status to 200 if registration is successful', async () => {
+    const ctx = ctxWithCredentials();
+    const mockStore = {
+      findOne: jest.fn().mockResolvedValue(null),
+      insertOne: jest.fn(),
+    };
+    const { register } = auth(mockStore);
+    await register(ctx, jest.fn());
+    expect(ctx.status).toBe(200);
+  });
+
+  it('should set status to 401 if user exists', async () => {
+    const ctx = ctxWithCredentials();
+    const mockStore = {
+      findOne: jest.fn().mockResolvedValue({}),
+      insertOne: jest.fn(),
+    };
+    const { register } = auth(mockStore);
+    await register(ctx, jest.fn());
     expect(ctx.status).toBe(401);
   });
+
+  it('should not register user if user already exists', async () => {
+    const ctx = ctxWithCredentials();
+    const mockStore = {
+      findOne: jest.fn().mockResolvedValue({}),
+      insertOne: jest.fn(),
+    };
+    const { register } = auth(mockStore);
+    await register(ctx, jest.fn());
+    expect(mockStore.insertOne).not.toHaveBeenCalled();
+  });
+
+  it('should not sign a token if user already exists', async () => {
+    const ctx = ctxWithCredentials();
+    const mockStore = {
+      findOne: jest.fn().mockResolvedValue({}),
+      insertOne: jest.fn(),
+    };
+    const { register } = auth(mockStore);
+    await register(ctx, jest.fn());
+    expect(jwt.sign).not.toHaveBeenCalled();
+  });
+
+  it('should have an empty body if user already exists', async () => {
+    const ctx = ctxWithCredentials();
+    const mockStore = {
+      findOne: jest.fn().mockResolvedValue({}),
+      insertOne: jest.fn(),
+    };
+    const { register } = auth(mockStore);
+    await register(ctx, jest.fn());
+    expect(ctx.body).toBeUndefined();
+  });
 });
 
-test('asdf', () => expect(1).toBe(1))
+describe('verify middleware', () => {
+  beforeEach(() => jest.clearAllMocks());
 
+  it('should set status to 401 if there is no authorization header', async () => {
+    const ctx = { request: {}, headers: {} };
+    const { verify } = auth(null);
+    await verify(ctx, jest.fn());
+    expect(ctx.status).toBe(401);
+  });
+
+  it('should set status to 401 if user is not found', async () => {
+    const ctx = ctxWithCredentials();
+    const { verify } = auth(null);
+    await verify(ctx, jest.fn());
+    expect(ctx.status).toBe(401);
+  });
+
+  it('should not call next if user is not found', async () => {
+    const ctx = ctxWithCredentials();
+    const { verify } = auth(null);
+    const mockNext = jest.fn();
+    await verify(ctx, mockNext);
+    expect(mockNext).not.toHaveBeenCalled();
+  });
+
+  it('should call next if user found and hashes match', async () => {
+    const ctx = ctxWithCredentials();
+    jwt.verify = jest.fn().mockResolvedValue(true);
+    jwt.decode = jest.fn().mockReturnValue({ username: 'david' });
+    const { verify } = auth(null);
+    const mockNext = jest.fn();
+    await verify(ctx, mockNext);
+    expect(mockNext).toHaveBeenCalledTimes(1);
+  });
+
+  it('should set ctx.params.username if user found and hashes match', async () => {
+    const ctx = ctxWithCredentials();
+    jwt.verify = jest.fn().mockResolvedValue(true);
+    jwt.decode = jest.fn().mockReturnValue({ username: 'david' });
+    const { verify } = auth(null);
+    const mockNext = jest.fn();
+    await verify(ctx, mockNext);
+    expect(ctx.params.username).toBe('david');
+  });
+
+  it('should not call next if hashes do not match', async () => {
+    const ctx = ctxWithCredentials();
+    jwt.verify = jest.fn().mockResolvedValue(false);
+    const { verify } = auth(null);
+    const mockNext = jest.fn();
+    await verify(ctx, mockNext);
+    expect(mockNext).not.toHaveBeenCalled();
+  });
+});
